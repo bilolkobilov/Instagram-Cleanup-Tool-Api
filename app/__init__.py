2e64f51edf25d16eb567eb59e5b89c00bf927fa5
bilolkobilov <bilolkobilov1@gmail.com>
2025-02-27 21:38:14 +0500

    Remove AfterRequest

--- app/__init__.py
+++ app/__init__.py
@@ -19,13 +19,5 @@ def create_app(config_object=None):
      supports_credentials=True)
    
     app.register_blueprint(api_bp, url_prefix='/api')
-    
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
-        response.headers.add('Access-Control-Allow-Credentials', 'true')
-        return response
    
     return app
